d30a9d4a91f315f2be275e2192143c41a3e29a49
rrenat358 <rrenat358@gmail.com>
2023-04-26 19:24:55 +0300

    1924

--- Lesson04-Spring-Boot/src/main/resources/index.js
+++ Lesson04-Spring-Boot/src/main/resources/index.js
@@ -1,12 +1,32 @@
 angular.module('app', []).controller('indexController', function ($scope, $http) {
-    const contextPach = 'http://localhost:8189/app';
-});
+    const contextPath = 'http://localhost:8189/app';
 
-$scope.loadStudents = function () {
-    $http.get(contextPach + '/students')
-        .then(function (response) {
-            $scope.StudentsList = response.data;
+    $scope.loadStudents = function () {
+        $http.get(contextPath + '/students')
+            .then(function (response) {
+                $scope.StudentsList = response.data;
+            });
+    };
+
+    $scope.deleteStudent = function (studentId) {
+        $http.get(contextPath + '/students/delete/' + studentId)
+            .then(function (response) {
+                $scope.loadStudents();
+            });
+    }
+
+    $scope.changeScore = function (studentId, delta) {
+        $http({
+            url: contextPath + '/students/change_score',
+            method: 'GET',
+            params: {
+                studentId: studentId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadStudents();
         });
+    }
 
     $scope.loadStudents();
-};
+});
