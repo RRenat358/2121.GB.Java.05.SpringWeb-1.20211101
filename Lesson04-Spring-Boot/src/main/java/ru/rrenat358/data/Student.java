6e5b7e10dfb8b327df6857ae3136c73d16365451
rrenat358 <rrenat358@gmail.com>
2023-04-26 19:55:26 +0300

    1955

--- Lesson04-Spring-Boot/src/main/java/ru/rrenat358/data/Student.java
+++ Lesson04-Spring-Boot/src/main/java/ru/rrenat358/data/Student.java
@@ -4,13 +4,45 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Student {
     private Long id;
     private String name;
     private Integer score;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer score) {
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
 
 }
