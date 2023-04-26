ef643a1ebdaed8397ffe6775607cedc1c59bee03
rrenat358 <rrenat358@gmail.com>
2023-04-26 19:16:36 +0300

    1916

--- Lesson04-Spring-Boot/src/main/java/ru/rrenat358/repositories/StudentRepository.java
+++ Lesson04-Spring-Boot/src/main/java/ru/rrenat358/repositories/StudentRepository.java
@@ -1,10 +1,42 @@
 package ru.rrenat358.repositories;
 
+import org.springframework.stereotype.Component;
 import ru.rrenat358.data.Student;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+
+@Component
 public class StudentRepository {
+    private List<Student> students;
+
+    @PostConstruct
+    public void init() {
+        students = new ArrayList<>(List.of(
+                new Student(1L, "Name01"),
+                new Student(2L, "Name02"),
+                new Student(3L, "Name03")
+        ));
+    }
+
+    public void add(Student student) {
+        students.add(student);
+    }
+
+
+
     public List<Student> getAllStudents() {
+
+        return Collections.unmodifiableList(students);
+    }
+
+    public Student findById(Long id) {
+        return students.stream()
+                .filter(student -> student.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(" -= Student not found =- "));
     }
 }
