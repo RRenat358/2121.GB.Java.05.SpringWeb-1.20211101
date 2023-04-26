ca77450e79a06f9366d9b6cba456f25493697abc
rrenat358 <rrenat358@gmail.com>
2023-04-26 19:02:10 +0300

    1902

--- Lesson04-Spring-Boot/src/main/java/ru/rrenat358/controllers/MainController.java
+++ Lesson04-Spring-Boot/src/main/java/ru/rrenat358/controllers/MainController.java
@@ -1,10 +1,25 @@
 package ru.rrenat358.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ru.rrenat358.data.Student;
 import ru.rrenat358.repositories.StudentRepository;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
     private StudentRepository studentRepository;
+
+    public MainController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @GetMapping("/students")
+    @ResponseBody
+    public List<Student> getAllStudents() {
+        return studentRepository.getAllStudents();
+    }
 }
--- Lesson04-Spring-Boot/src/main/java/ru/rrenat358/repositories/StudentRepository.java
+++ Lesson04-Spring-Boot/src/main/java/ru/rrenat358/repositories/StudentRepository.java
@@ -1,4 +1,10 @@
 package ru.rrenat358.repositories;
 
+import ru.rrenat358.data.Student;
+
+import java.util.List;
+
 public class StudentRepository {
+    public List<Student> getAllStudents() {
+    }
 }
