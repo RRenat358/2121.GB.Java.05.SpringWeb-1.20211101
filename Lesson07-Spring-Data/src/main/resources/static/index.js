50868ebf5aff9d2846ed8c8c93962ebdeafecbb3
rrenat358 <rrenat358@gmail.com>
2023-04-30 17:49:55 +0300

    1749

---
+++ Lesson07-Spring-Data/src/main/resources/static/index.js
@@ -0,0 +1,32 @@
+angular.module('app', []).controller('indexController', function ($scope, $http) {
+    const contextPath = 'http://localhost:8189/app';
+
+    $scope.loadStudents = function () {
+        $http.get(contextPath + '/students')
+            .then(function (response) {
+                $scope.StudentsList = response.data;
+            });
+    };
+
+    $scope.deleteStudent = function (studentId) {
+        $http.get(contextPath + '/students/delete/' + studentId)
+            .then(function (response) {
+                $scope.loadStudents();
+            });
+    }
+
+    $scope.changeScore = function (studentId, delta) {
+        $http({
+            url: contextPath + '/students/change_score',
+            method: 'GET',
+            params: {
+                studentId: studentId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadStudents();
+        });
+    }
+
+    $scope.loadStudents();
+});
