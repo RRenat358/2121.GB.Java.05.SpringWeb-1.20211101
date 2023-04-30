6ee8f051d2e1c9f730eab3ecf450dc06da5c3d06
rrenat358 <rrenat358@gmail.com>
2023-04-30 21:55:29 +0300

    2154

---
+++ Lesson07-Spring-Data/src/main/java/ru/rrenat358/entities/Student.java
@@ -0,0 +1,45 @@
+package ru.rrenat358.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "score")
+    private Integer score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Student() {
+    }
+}
