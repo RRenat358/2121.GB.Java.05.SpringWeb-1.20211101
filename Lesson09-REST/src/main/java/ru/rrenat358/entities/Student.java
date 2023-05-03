f6cea583861d4dafd82b25a2668b661d346574f6
rrenat358 <rrenat358@gmail.com>
2023-05-03 09:04:12 +0300

    0904

---
+++ Lesson09-REST/src/main/java/ru/rrenat358/entities/Student.java
@@ -0,0 +1,56 @@
+package ru.rrenat358.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @Column(name = "secret_key")
+    private String secretKey;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public String getSecretKey() {
+        return secretKey;
+    }
+
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public Student() {
+    }
+}
