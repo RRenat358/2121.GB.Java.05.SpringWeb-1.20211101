5af9ea2b030cf0b29f208a476fc8fb9e870452ee
rrenat358 <rrenat358@gmail.com>
2023-05-05 17:03:21 +0300

    1702

---
+++ Lesson12-SpringWeb-FinalBuild/src/main/java/ru/rrenat358/SpringWebApplication.java
@@ -0,0 +1,15 @@
+package ru.rrenat358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebApplication {
+	// Домашнее задание:
+	// 1. * Добавить регистрацию новых пользователей
+	// 2. Если п. 1 очень сложным показался, то хотя бы текстом опишите план действий
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebApplication.class, args);
+	}
+}
