d51d7120bcd41e0a44f362ab77e3be7b43f40a99
rrenat358 <rrenat358@gmail.com>
2023-05-05 10:41:11 +0300

    1041

---
+++ Lesson11-Spring-Security/jdbc/src/main/java/ru/rrenat358/SpringSecurityJdbcApplication.java
@@ -0,0 +1,11 @@
+package ru.rrenat358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJdbcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJdbcApplication.class, args);
+    }
+}
