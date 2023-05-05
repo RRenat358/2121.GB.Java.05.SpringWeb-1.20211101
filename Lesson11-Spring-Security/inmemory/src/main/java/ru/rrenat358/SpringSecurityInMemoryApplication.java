33a4a0d7a099c05168c3a0de92a753d002df1a67
rrenat358 <rrenat358@gmail.com>
2023-05-05 10:35:33 +0300

    1032

---
+++ Lesson11-Spring-Security/inmemory/src/main/java/ru/rrenat358/SpringSecurityInMemoryApplication.java
@@ -0,0 +1,11 @@
+package ru.rrenat358;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityInMemoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityInMemoryApplication.class, args);
+    }
+}
