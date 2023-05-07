4fb62ebe36d5ab9b16126685b410af4ff78c030c
rrenat358 <rrenat358@gmail.com>
2023-05-07 18:58:51 +0300

    1858

---
+++ Lesson03-Spring-MVC/src/main/java/ru.rrenat358/data/Student.java
@@ -0,0 +1,30 @@
+package ru.rrenat358.data;
+
+public class Student {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Student() {
+    }
+
+    public Student(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
