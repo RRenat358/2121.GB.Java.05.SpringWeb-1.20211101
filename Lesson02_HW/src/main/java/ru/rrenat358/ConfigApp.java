0e3daa54ba00203e24d81bd2d0a76546b0a4a71c
rrenat358 <rrenat358@gmail.com>
2023-05-09 11:01:11 +0300

    1101

---
+++ Lesson02_HW/src/main/java/ru/rrenat358/ConfigApp.java
@@ -0,0 +1,11 @@
+package ru.rrenat358;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.rrenat358")
+public class ConfigApp {
+
+}
---
+++ Lesson02_HW/src/main/java/ru/rrenat358/ReMarketApp.java
@@ -0,0 +1,13 @@
+package ru.rrenat358;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ReMarketApp {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ConfigApp.class);
+
+
+    }
+
+}
