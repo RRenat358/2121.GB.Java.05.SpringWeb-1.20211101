a434265718a6157a6c5defaae9e056a0656f1b5a
rrenat358 <rrenat358@gmail.com>
2023-05-10 21:04:01 +0300

    2103

--- Lesson03_HW/src/main/java/ru/rrenat358/entity/Product.java
+++ Lesson03_HW/src/main/java/ru/rrenat358/entity/Product.java
@@ -1,4 +1,4 @@
-package ru.rrenat358;
+package ru.rrenat358.entity;
 
 
 public class Product {
---
+++ Lesson03_HW/src/main/java/ru/rrenat358/repository/ProductRepository.java
@@ -0,0 +1,40 @@
+package ru.rrenat358.repository;
+
+import org.springframework.stereotype.Component;
+import ru.rrenat358.entity.Product;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+
+    private List<Product> products;
+
+
+    @PostConstruct
+    private void init() {
+        products = new ArrayList<>(List.of(
+                new Product(1L, "Яблоки"),
+                new Product(2L, "Бананы"),
+                new Product(3L, "Груши"),
+                new Product(4L, "Сливы"),
+                new Product(5L, "Мороковь")
+        ));
+    }
+
+    public List<Product> findAll() {
+        return Collections.unmodifiableList(products);
+    }
+
+    public Product findById(Long id) {
+        return products.stream()
+                .filter(product -> products.equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException());
+    }
+
+
+}
