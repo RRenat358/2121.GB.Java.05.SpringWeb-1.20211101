fce7c42dad10fa90f20c132102195e2198432d4c
rrenat358 <rrenat358@gmail.com>
2023-05-11 15:28:48 +0300

    1528

--- Lesson04_HW/src/main/java/ru/rrenat358/controllers/ProductController.java
+++ Lesson04_HW/src/main/java/ru/rrenat358/controllers/ProductController.java
@@ -4,6 +4,8 @@ package ru.rrenat358.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.rrenat358.entities.Product;
 import ru.rrenat358.services.ProductService;
@@ -26,8 +28,16 @@ public class ProductController {
     }
 
 
+    @GetMapping("/products/delete/{id}")
+    public void deleteById(@PathVariable Long id) {
+        productService.deleteById(id);
+    }
 
 
-
-
+    //todo
+    @GetMapping("/products/change_score")
+    public void changeScore(@RequestParam Long productId, @RequestParam Integer delta) {
+        Product p = productService.findById(productId);
+        p.getPrice();
+    }
 }
