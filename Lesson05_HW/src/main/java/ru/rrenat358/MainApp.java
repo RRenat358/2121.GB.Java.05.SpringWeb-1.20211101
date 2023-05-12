e8b88b9dd70a3201015d7841871e1a4f9724de33
rrenat358 <rrenat358@gmail.com>
2023-05-12 18:54:48 +0300

    1854

--- Lesson05_HW/src/main/java/ru/rrenat358/MainApp.java
+++ Lesson05_HW/src/main/java/ru/rrenat358/MainApp.java
@@ -4,6 +4,17 @@ package ru.rrenat358;
 
 public class MainApp {
     public static void main(String[] args) {
+        SessionFactoryUtils sessionFactoryUtils = new SessionFactoryUtils();
+        sessionFactoryUtils.init();
+        try {
+            ProductDao userDao = new ProductDaoImpl(sessionFactoryUtils);
+//            userDao.testCaching();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            sessionFactoryUtils.shutdown();
+        }
 
     }
 }
--- Lesson05_HW/src/main/java/ru/rrenat358/ProductDaoImpl.java
+++ Lesson05_HW/src/main/java/ru/rrenat358/ProductDaoImpl.java
@@ -3,6 +3,12 @@ package ru.rrenat358;
 
 public class ProductDaoImpl implements ProductDao {
 
+    SessionFactoryUtils sessionFactoryUtils;
+
+    public ProductDaoImpl(SessionFactoryUtils sessionFactoryUtils) {
+        this.sessionFactoryUtils = sessionFactoryUtils;
+    }
+
     @Override
     public Product findById(Long id) {
         return new Product();
