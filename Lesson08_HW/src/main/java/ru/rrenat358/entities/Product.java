0b5bc9fda176b8e4ecd7ac8cbe68b6ba0c5fe1bc
rrenat358 <rrenat358@gmail.com>
2023-05-15 22:24:29 +0300

    2224

---
+++ Lesson08_HW/src/main/java/ru/rrenat358/entities/Product.java
@@ -0,0 +1,31 @@
+package ru.rrenat358.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+
+    @Column(name = "price")
+    Integer price;
+
+
+
+}
