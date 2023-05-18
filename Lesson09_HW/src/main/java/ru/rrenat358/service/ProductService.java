e314794646bbe3d4ee9fe7843bb2290565b00cd4
rrenat358 <rrenat358@gmail.com>
2023-05-18 13:14:42 +0300

    1314

---
+++ Lesson09_HW/src/main/java/ru/rrenat358/service/ProductService.java
@@ -0,0 +1,63 @@
+package ru.rrenat358.service;
+
+import org.springframework.stereotype.Service;
+import ru.rrenat358.entities.Product;
+import ru.rrenat358.repositories.ProductRepository;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> findById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        productRepository.deleteById(id);
+    }
+
+    // NoUsed
+//    @Transactional
+//    public void changePrice(Long productId, Integer delta) {
+//        Product product = productRepository.findById(productId)
+//                .orElseThrow(()-> new ResourceNotFoundException("Продукт не найден для ID : " + productId));
+//        product.setPrice(product.getPrice() + delta);
+//    }
+
+    @Transactional
+    public void changePriceToDelta(Long id, Integer delta) {
+        productRepository.changePriceToDelta(id, delta);
+    }
+
+
+    public List<Product> findAllByPriceBetween(Integer min, Integer max) {
+        return productRepository.findAllByPriceBetween(min, max);
+    }
+
+    public List<Product> findAllByPriceBelowLimit(Integer maxLimit) {
+        return productRepository.findAllByPriceBelowLimit(maxLimit);
+    }
+
+    public List<Product> findAllByPriceAboveLimit(Integer minLimit) {
+        return productRepository.findAllByPriceAboveLimit(minLimit);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+}
