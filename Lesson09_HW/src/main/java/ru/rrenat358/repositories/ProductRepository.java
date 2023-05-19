25b55c7aa0731f6d9e550a65bf74e63cc15d283c
rrenat358 <rrenat358@gmail.com>
2023-05-19 10:06:02 +0300

    1005

--- Lesson09_HW/src/main/java/ru/rrenat358/repositories/ProductRepository.java
+++ Lesson09_HW/src/main/java/ru/rrenat358/repositories/ProductRepository.java
@@ -1,13 +1,14 @@
 package ru.rrenat358.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ru.rrenat358.entities.Product;
 
 import java.util.List;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 
     List<Product> findAllByPriceBetween(Integer min, Integer max);
 
---
+++ Lesson09_HW/src/main/java/ru/rrenat358/repositories/specifications/ProductSpecifications.java
@@ -0,0 +1,19 @@
+package ru.rrenat358.repositories.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+import ru.rrenat358.entities.Product;
+
+public class ProductSpecifications {
+
+    public static Specification<Product> scoreGreaterOrEqualsThan(Integer price) {
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.greaterThanOrEqualTo(root.get("price"), price);
+    }
+
+    public static Specification<Product> scoreLessThanOrEqualsThan(Integer price) {
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.lessThanOrEqualTo(root.get("price"), price);
+    }
+
+    public static Specification<Product> nameLike(String namePart) {
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("name"), String.format("%%%s%%", namePart));
+    }
+}
