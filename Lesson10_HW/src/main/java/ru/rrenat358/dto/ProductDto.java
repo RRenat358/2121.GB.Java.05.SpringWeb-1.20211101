5349a9de6b63aa85b54800695cf7defd7bc14156
rrenat358 <rrenat358@gmail.com>
2023-05-21 15:01:25 +0300

    1501

--- Lesson10_HW/src/main/java/ru/rrenat358/dto/ProductDto.java
+++ Lesson10_HW/src/main/java/ru/rrenat358/dto/ProductDto.java
@@ -1,12 +1,14 @@
 package ru.rrenat358.dto;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import ru.rrenat358.entities.Product;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductDto {
     Long id;
     String name;
