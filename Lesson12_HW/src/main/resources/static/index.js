4dbf15b5e6e6ac50521751dabd768f8046bcfe34
rrenat358 <rrenat358@gmail.com>
2023-05-30 14:55:32 +0300

    1455

--- Lesson12_HW/src/main/resources/static/index.js
+++ Lesson12_HW/src/main/resources/static/index.js
@@ -1,4 +1,4 @@
-angular.module('app', []).controller('indexController', function ($scope, $http) {
+angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $rootScope, $http, $localStorage) {
     const contextPath = 'http://localhost:8189/app/api/v1';
 
 
@@ -81,6 +81,33 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
         });
     }
 */
+
+    $scope.tryToAuth = function () {
+        $http.post('http://localhost:8189/app/auth', $scope.user)
+            .then(function successCallback(response) {
+                if (response.data.token) {
+                    $http.defaults.headers.common.Authorization = 'Bearer ' + response.data.token;
+                    $localStorage.springWebUser = {username: $scope.user.username, token: response.data.token};
+
+                    $scope.user.username = null;
+                    $scope.user.password = null;
+                }
+            }, function errorCallback(response) {
+            });
+    };
+
+    $scope.tryToLogout = function () {
+        $scope.clearUser();
+        if ($scope.user.username) {
+            $scope.user.username = null;
+        }
+        if ($scope.user.password) {
+            $scope.user.password = null;
+        }
+    };
+
+
+
 
 
     $scope.loadProduct();
